fe4f1e31cc6654935c341d8fd851a85dbc5b6984
khacdai24 <khacdai24moon@gmail.com>
2025-11-27 01:33:41 +0700

    First release of thuchanh

---
+++ login.py
@@ -0,0 +1,59 @@
+import tkinter.messagebox as messagebox
+import tkinter as tk
+import pandas as pd
+import csv
+import os
+
+window = tk.Tk()
+window.title("Login/Reg")
+window.geometry("500x500")
+
+user = ""
+pas = ""
+
+def get_info():
+    global user, pas
+    user = user_entry.get().strip()
+    pas = pass_entry.get().strip()
+    if user=="" or pas=="":
+        messagebox.showerror("Error", "Fill Information")
+        return False
+    return True
+
+def login_function():
+    if not get_info(): return
+    dt = pd.read_csv("dulieu.csv")
+
+    if ((dt["username"]==user).values == [False]):
+        messagebox.showerror("Error", "Can not find Username")
+    elif ((dt["username"]==user).values != [False]) and ((dt["passw"]==pas).values == [False]):
+        messagebox.showerror("Error", "Wrong Password!")
+    else:
+        messagebox.showinfo("Information", "Login Successfully!")
+
+def reg_function():
+    if get_info(): return
+    dt = pd.read_csv("dulieu.csv")
+    if ((dt["username"]==user).values == [False]):
+        with open("dulieu.csv", "a", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow([user, pas])
+        messagebox.showinfo("Information", "Reg Successfully!")
+    else:
+        messagebox.showerror("Error", "Exist Username!")
+
+tk.Label(window, text='username', font=('Arial', 15)).grid(row=0, column=0, padx=20)
+user_entry = tk.Entry(window, font=('Arial', 15))
+user_entry.grid(row=0, column=1)
+
+tk.Label(window, text='password', font=('Arial', 15)).grid(row=1, column=0, padx=20, pady=20)
+pass_entry = tk.Entry(window, font=('Arial', 15))
+pass_entry.grid(row=1, column=1, pady=20)
+
+login_button = tk.Button(window, text="Login", font=('Arial', 15), command=login_function)
+login_button.grid(row=2, column=1, padx=(0,100))
+
+reg_button = tk.Button(window, text="Reg", font=('Arial', 15), command=reg_function)
+reg_button.grid(row=2, column=2, padx=10)
+
+window.mainloop()
---
+++ save_data_tkinter_to_csv.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+import csv
+import tkinter.messagebox as messagebox
+import os
+
+def enter_data_function():
+    name = name_entry.get().strip()
+    age = age_entry.get().strip()
+    email = email_entry.get().strip()
+    
+    if name == "" or age == "" or email == "": 
+        messagebox.showerror("Error", "Please Fill Information")
+        return
+
+    checkfile = os.path.isfile("dulieu.csv")
+    with open("dulieu.csv", "a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+
+        if not checkfile:
+            writer.writerow(["Name", "Age", "Email"])
+        
+        writer.writerow([name, age, email])
+    
+    messagebox.showinfo("Infomation", "Save Data Successfully")
+    
+    return 
+
+window = tk.Tk()
+window.title("Save data")
+window.geometry("600x600")
+
+tk.Label(window, text="Name", fg="white", bg="limegreen", font=("Arial", 15)).pack(anchor="w", padx=100)
+name_entry = tk.Entry(window, width=20, font=("Arial", 15))
+name_entry.pack(padx=20, pady=0)
+
+tk.Label(window, text="Age", fg="white", bg="limegreen", font=("Arial", 15)).pack(anchor="w", padx=100)
+age_entry = tk.Entry(window, width=20, font=("Arial", 15))
+age_entry.pack(padx=20, pady=0)
+
+tk.Label(window, text="Email", fg="white", bg="limegreen", font=("Arial", 15)).pack(anchor="w", padx=100)
+email_entry = tk.Entry(window, width=20, font=("Arial", 15))
+email_entry.pack()
+    
+save_button = tk.Button(window, text="Enter", fg="white", bg="cyan", font=("Arial", 15), command=enter_data_function)
+save_button.pack(pady=20)
+
+window.mainloop()
